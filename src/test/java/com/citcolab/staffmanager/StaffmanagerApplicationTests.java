52cd54b0ca69976cc38a71c4a0370213cbb8ddae
Reis-Silva J.C <cesar.everlastingtechnologydeveloper@outlook.com>
2021-03-08 18:01:16 -0300

    Add files via upload

---
+++ src/test/java/com/citcolab/staffmanager/StaffmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citcolab.staffmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
