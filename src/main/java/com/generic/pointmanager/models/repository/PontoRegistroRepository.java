be7820ca8cc008227071becf124a7030a4ed24d3
Reis-Silva J.C <julio.silva@vibetecnologia.com>
2021-04-05 21:45:06 -0300

    melhorias para solicitação de atualizar pontos

--- src/main/java/com/generic/pointmanager/models/repository/PontoRegistroRepository.java
+++ src/main/java/com/generic/pointmanager/models/repository/PontoRegistroRepository.java
@@ -1,10 +1,12 @@
 package com.generic.pointmanager.models.repository;
 
+import java.math.BigInteger;
 import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import com.generic.pointmanager.models.entity.PontoRegistro;
 
@@ -14,10 +16,10 @@ public interface PontoRegistroRepository extends JpaRepository<PontoRegistro, Lo
 	 List<PontoRegistro> buscarPorDataHoraPonto(Date dataPonto, Long idUsuario);
 	 
 	 @Query(value = "SELECT * FROM administrativo.ponto_registro where id_usuario = ?", nativeQuery = true)
-	 List<PontoRegistro> buscarPorUsuarioId(Long id);
+	 List<PontoRegistro> buscarPorUsuarioId(BigInteger id);
 	 
-	 @Query(value = "SELECT * FROM administrativo.ponto_registro where id_usuario = ? and Date(data_ponto) between ? and ?", nativeQuery = true)
-	 List<PontoRegistro> buscarPorUsuarioIdEspecifico(Long idUsuario, Date datainicial, Date datafinal);
+	 @Query(value = "SELECT * FROM administrativo.ponto_registro where id_usuario = ? and Date(data_ponto) between ? and ? ", nativeQuery = true)
+	 List<PontoRegistro> buscarPorUsuarioIdEspecifico(Long idUsuario, @DateTimeFormat(pattern="yyyy-MM-dd") Date datainicial, @DateTimeFormat(pattern="yyyy-MM-dd") Date datafinal);
 	 
 	 @Query(value = "SELECT * FROM administrativo.ponto_registro where id_usuario = ? and id_ponto_registro = ?", nativeQuery = true)
 	 PontoRegistro buscarPorUsuarioIdAndPontoRegistroId(Long idUsuario, Long idPontoRegistro);
--- src/main/java/com/generic/pointmanager/service/impl/AtualizarPontoRegistroServiceImpl.java
+++ src/main/java/com/generic/pointmanager/service/impl/AtualizarPontoRegistroServiceImpl.java
@@ -3,25 +3,34 @@ package com.generic.pointmanager.service.impl;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 
 import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.generic.pointmanager.enumeration.StatusAtualizacaoPonto;
 import com.generic.pointmanager.enumeration.StatusSolicitacaoAtualizacaoPonto;
 import com.generic.pointmanager.exception.ValidarCpfException;
 import com.generic.pointmanager.exception.ValidarDataException;
 import com.generic.pointmanager.exception.ValidarNivelAcessoException;
+import com.generic.pointmanager.exception.ValidarPontoRegistroException;
 import com.generic.pointmanager.models.entity.AtualizarPontoRegistro;
+import com.generic.pointmanager.models.entity.CollectionRegistro;
 import com.generic.pointmanager.models.entity.PontoRegistro;
 import com.generic.pointmanager.models.entity.Registro;
 import com.generic.pointmanager.models.entity.Usuario;
@@ -31,8 +40,11 @@ import com.generic.pointmanager.models.repository.RegistroRepository;
 import com.generic.pointmanager.models.repository.UsuarioRepository;
 import com.generic.pointmanager.service.AtualizarPontoRegistroService;
 import com.generic.pointmanager.ws.GerenciadorRepositoryService;
+import com.generic.pointmanager.ws.impl.GetCollection;
 
-@RequestMapping("/atualizadarPontoRegistro")
+@Service
+@RestController
+@RequestMapping("/atualizarPontoRegistro")
 public class AtualizarPontoRegistroServiceImpl implements AtualizarPontoRegistroService{
 	
 	@Autowired
@@ -47,13 +59,16 @@ public class AtualizarPontoRegistroServiceImpl implements AtualizarPontoRegistro
 	@Autowired
 	private AtualizarPontoRegistroRepository atualizarPontoRegistroRepository;
 	
+	@Autowired
 	private RegistroRepository registroRepository;
 	
-	@PostMapping("/solicitar/{idUsuario}/{idPontoRegistro}")
-	@Transactional
+	private PontoRegistroServiceImpl pontoRegistroServiceImpl;
+	
+	@PostMapping("/solicitar")
 	@Override
-	public ResponseEntity solicitarAtualizarPonto(@RequestBody List<AtualizarPontoRegistro> atualizarPontoRegistro, 
-			@RequestBody List<PontoRegistro> pontoRegistro, @RequestBody Registro registro) {
+	public ResponseEntity solicitarAtualizarPonto(@RequestBody GetCollection collection) {
+			
+		ValidarPontoRegistroException.validarPontoRegistroNumero(collection.getCollection().getPontoRegistro(), collection.getCollection().getAtualizarPontoRegistro());
 		
 		BigInteger numeroRegistro = registroRepository.maiorNumeroRegistro();
 		
@@ -61,28 +76,31 @@ public class AtualizarPontoRegistroServiceImpl implements AtualizarPontoRegistro
 			numeroRegistro = BigInteger.valueOf(1);
 		}
 		
-		registro.setNumeroRegistro(numeroRegistro);
+		Calendar dataAtual = Calendar.getInstance();
+		
+		collection.getCollection().getRegistro().setNumeroRegistro(numeroRegistro);
+		collection.getCollection().getRegistro().setDataSolicitacao(dataAtual.getTime());
+		
+		gerenciadorRepositoryService.persitirRegistro(collection.getCollection().getRegistro());
 		
 		AtualizarPontoRegistro solicitarAtualizacao = new AtualizarPontoRegistro();
 		
-		Calendar dataAtual = Calendar.getInstance();
-		
-		for(int index=0; index == pontoRegistro.size()-1; index++) {
+		for(int index=0; index < collection.getCollection().getPontoRegistro().length; index++) {
 			
-			if(pontoRegistro.get(index).getHoraPonto() != solicitarAtualizacao.getHoraPontoPosterior()) {
-				solicitarAtualizacao.setHoraPontoPosterior(atualizarPontoRegistro.get(index).getHoraPontoPosterior());
-				solicitarAtualizacao.setDataSolicitacao(dataAtual.getTime());
-				solicitarAtualizacao.getIdPontoRegistro().setId(pontoRegistro.get(index).getId());
+			if(collection.getCollection().getPontoRegistro()[index].getHoraPonto().compareTo(
+					collection.getCollection().getAtualizarPontoRegistro()[index].getHoraPontoPosterior()) !=0) {
+				solicitarAtualizacao.setHoraPontoPosterior(collection.getCollection().getAtualizarPontoRegistro()[index].getHoraPontoPosterior());
+				solicitarAtualizacao.setIdPontoRegistro(new PontoRegistro());
+				solicitarAtualizacao.getIdPontoRegistro().setId(collection.getCollection().getPontoRegistro()[index].getId());
 				solicitarAtualizacao.setStatusSolicitacaoAtualizacaoPonto(StatusSolicitacaoAtualizacaoPonto.PENDENTE);
+				solicitarAtualizacao.setStatusAtualizacaoPonto(StatusAtualizacaoPonto.PENDENTE);
 				solicitarAtualizacao.setNumeroRegistro(numeroRegistro);
 				
 				gerenciadorRepositoryService.persistirAtualizarPontoRegistro(solicitarAtualizacao);
 			}
 		}
 		
-		gerenciadorRepositoryService.persitirRegistro(registro);
-
-		return ResponseEntity.ok(atualizarPontoRegistro);
+		return ResponseEntity.noContent().build();
 	}	
 	
 	@GetMapping("/procurargeral/{idAdmin}")
@@ -96,9 +114,9 @@ public class AtualizarPontoRegistroServiceImpl implements AtualizarPontoRegistro
 				atualizarPontoRegistroRepository.buscarAtualizarListaGeralPontoRegistro());
 	}
 	
-	@GetMapping("/procurarespecificousuario/{idAdmin}")
+	@GetMapping("/procurarespecificousuario/{idAdmin}/{cpf}")
 	@Override
-	public ResponseEntity listaAtualizarPontoEspecifico(String cpf, @PathVariable("idAdmin") Long idAdmin) {
+	public ResponseEntity listaAtualizarPontoEspecifico(@PathVariable("idAdmin") Long idAdmin, @PathVariable("cpf") String cpf) {
 		
 		ValidarCpfException.validarCPF(usuarioRepository.existsByCpf(cpf), "find");
 		
@@ -109,18 +127,36 @@ public class AtualizarPontoRegistroServiceImpl implements AtualizarPontoRegistro
 				atualizarPontoRegistroRepository.buscarAtualizarListaUsuarioPontoRegistroEspecifico(usuarioRepository.findByCpf(cpf).getId()));
 	}
 	
+	@GetMapping("/listaVisaoPontosDiariosConjuntoAtualizar/{idAdmin}/{idAtualizarPontoRegistro}")
 	@Override
-	public ResponseEntity atualizarPonto(Long idAtualizarPontoRegistro, @PathVariable("idAdmin") Long idAdmin,
+	public ResponseEntity listaVisaoPontosDiariosConjuntoAtualizar(@PathVariable("idAdmin") Long idAdmin,
+			@PathVariable("idAtualizarPontoRegistro") Long idAtualizarPontoRegistro) {
+		
+		Optional<AtualizarPontoRegistro> atualizacao = atualizarPontoRegistroRepository.findById(idAtualizarPontoRegistro);
+		
+		Optional<PontoRegistro> pontoRegistroAtualizar = pontoRegistroRepository.findById(atualizacao.get().getId());
+		
+		List<PontoRegistro> pontosdiariosAtualizacao = new ArrayList<PontoRegistro>();
+		
+		
+		pontosdiariosAtualizacao.addAll((Collection<? extends PontoRegistro>) pontoRegistroServiceImpl.procurarPontoEspecifico(
+				pontoRegistroAtualizar.get().getId(), pontoRegistroAtualizar.get().getDataPonto(), pontoRegistroAtualizar.get().getDataPonto()));
+		
+		return ResponseEntity.ok(pontosdiariosAtualizacao);
+	}
+	
+	@Override
+	public ResponseEntity atualizarPonto(@PathVariable("idAdmin") Long idAdmin, @PathVariable("idAtualizarPontoRegistro") Long idAtualizarPontoRegistro,
 			AtualizarPontoRegistro atualizarPontoRegistro) {
 		
-		Optional<AtualizarPontoRegistro> Atualizacao = atualizarPontoRegistroRepository.findById(idAtualizarPontoRegistro);
+		Optional<AtualizarPontoRegistro> atualizacao = atualizarPontoRegistroRepository.findById(idAtualizarPontoRegistro);
 		
-		ValidarNivelAcessoException.validarNivelAcesso(
-				usuarioRepository.findById(idAdmin).get().getNivelAcesso().getDescricao(), );
-		
-		ValidarDataException.validarPontoDataHora(pontoRegistroAtualizar, null, null);
+		Optional<PontoRegistro> pontoRegistroAtualizar = pontoRegistroRepository.findById(atualizacao.get().getId());
 		
 		Calendar dataAtual = Calendar.getInstance();
+		
+		pontoRegistroAtualizar.get().setHoraPonto(atualizacao.get().getHoraPontoPosterior());
+		
 		
 		Atualizacao.setHoraPontoPosterior(pontoRegistroAtualizar.getHoraPonto());
 		Atualizacao.setDataSolicitacao(dataAtual.getTime());
@@ -132,8 +168,6 @@ public class AtualizarPontoRegistroServiceImpl implements AtualizarPontoRegistro
 
 		return ResponseEntity.ok(solicitarAtualizacao);
 		
-		return null;
 	}
-
 		
 }
